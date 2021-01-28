509998a454de9dbf9db306f03bc2e4160c1cc20f
aleosina12 <68166171+aleosina12@users.noreply.github.com>
2021-01-28 22:11:07 +0300

    Delete bootstrap.php

--- bootstrap.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once 'C:\dev\Skillfactory\Module_26_MVC_NEW\application\core\model.php';
-require_once 'C:\dev\Skillfactory\Module_26_MVC_NEW\application\core\view.php';
-require_once 'C:\dev\Skillfactory\Module_26_MVC_NEW\application\core\controller.php';
-require_once 'C:\dev\Skillfactory\Module_26_MVC_NEW\application\core\route.php';
-
-Route::start(); // запускаем маршрутизатор
